0313b535ab02a72f3621f7323e670557ca7b32c7
Taha <taham8000@gmail.com>
2026-01-02 15:28:15 +0330

    add sample monitor and client

---
+++ broker/clinet.py
@@ -0,0 +1,26 @@
+import paho.mqtt.client as mqtt
+import time
+from dotenv import load_dotenv
+import os 
+load_dotenv()
+
+
+public_ip = os.getenv("IP", "localhost")
+
+
+client = mqtt.Client()
+client.connect(public_ip, 1883, 60)
+
+
+
+test_payload_en = "+989121234567:Hello from MC60!"
+client.publish("device/MC60/sms_rx", test_payload_en)
+print("📤 Sent English Mock Data")
+
+time.sleep(1)
+
+test_payload_fa = "+989121234567:0633064406270645"
+client.publish("device/MC60/sms_rx", test_payload_fa)
+print("📤 Sent Persian (Hex) Mock Data")
+
+client.disconnect()
---
+++ broker/monitor.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+public_ip = os.getenv("IP", "localhost")
+
+
+def on_connect(client, userdata, flags, rc):
+    print(f"✅ Monitor connected with result code {rc}")
+    client.subscribe("device/MC60/sms_rx")
+
+def on_message(client, userdata, msg):
+    print(f"🔔 New SMS Data Received!")
+    print(f"Topic: {msg.topic}")
+    print(f"Payload: {msg.payload.decode()}")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(public_ip, 1883, 60)
+client.loop_forever()
+
